41a524045c59d3111af0e5bff3fefd90cf40c8a0
Chris Kavanagh <ckava3@gmail.com>
2022-02-13 20:09:15 -0500

    routes

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,12 @@
-console.log("Hello World");
+import express from "express";
+import goals from "./routes/goals.js";
+//const express = require("express");
+//const dotenv = require("dotenv").config();
+import "dotenv/config";
+
+const app = express();
+const port = process.env.PORT || 5000;
+app.use(express.urlencoded({ extended: false }));
+
+app.use("/api/goals", goals);
+app.listen(port, () => console.log(`Server Started Port ${port}`));
