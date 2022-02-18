0cead6930cdb1f53a8397587577528a6e2434cee
Chris Kavanagh <ckava3@gmail.com>
2022-02-17 23:01:09 -0500

    routes clean up

--- backend/routes/goalRoutes.js
+++ backend/routes/goalRoutes.js
@@ -7,13 +7,13 @@ import {
 } from "../controllers/goalController.js";
 const router = express.Router();
 
-router.get("/", getGoals);
+router.route("/").get(getGoals).post(setGoal);
+router.route("/:id").delete(deleteGoal).put(updateGoal);
 
+/* router.get("/", getGoals);
 router.post("/", setGoal);
-
 router.put("/:id", updateGoal);
-
-router.delete("/:id", deleteGoal);
+router.delete("/:id", deleteGoal); */
 
 //module.exports = router;
 //export { router };
